a4db1f8d202f1c2771eeae9a1ec0aed5820fb619
jiahui.dong <jiahui.dong@beibei.com>
2018-11-12 14:41:40 +0800

    Initial commit

---
+++ src/main/java/com/jiahui/nbarobot/NbarobotApplication.java
@@ -0,0 +1,12 @@
+package com.jiahui.nbarobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbarobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbarobotApplication.class, args);
+	}
+}
