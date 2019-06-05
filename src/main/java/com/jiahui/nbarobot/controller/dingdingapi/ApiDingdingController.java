b07389757484e12214d085b6cb29002908517df5
jiahui.dong <jiahui.dong@beibei.com>
2019-06-05 14:57:00 +0800

    上传钉钉测试机器人代码

--- src/main/java/com/jiahui/nbarobot/controller/dingdingapi/ApiDingdingController.java
+++ src/main/java/com/jiahui/nbarobot/controller/dingdingapi/ApiDingdingController.java
@@ -7,9 +7,12 @@ import com.jiahui.nbarobot.domain.dingding.TextMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author dongjiahui
@@ -22,10 +25,10 @@ public class ApiDingdingController {
 
     @RequestMapping(value = "/qc",produces="application/json;charset=UTF-8",method = RequestMethod.POST)
     @ResponseBody
-    public String qc(CallbackRequest request){
+    public String qc(@RequestBody CallbackRequest request){
 
         logger.info("qc 接收到机器人信息:" +request.toString());
-        DingtalkMessage message = new TextMessage("李月芹是🐷");
+        DingtalkMessage message = new TextMessage(request.getSenderNick() + "你是🐷");
         return message.toJsonString();
     }
 }
